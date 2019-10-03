0a60ec22448ea9183efda54e9598f34a527a09ce
francisco cabrera <fjcabrera@buffalo.edu>
2019-10-03 17:25:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,12 @@
 def create_an_empty_array
    []
 
-  
+
 end
 
 def create_an_array
   [1,2,3,4]
-  
+
 end
 
 def add_element_to_end_of_array(array,element)
@@ -25,26 +25,26 @@ def remove_element_from_end_of_array(array)
    array = ["wow", "I", "am", "really", "learning"]
     element = "arrays!"
     array << element
-  
+
 end
 
 def remove_element_from_start_of_array(array)
    array = ["wow", "I", "am", "really", "learning"]
     element = "arrays!"
     array << element
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
