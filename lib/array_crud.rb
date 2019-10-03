edff258eeb8c5ad47d1351144c0fd54687670bd5
francisco cabrera <fjcabrera@buffalo.edu>
2019-10-03 16:53:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,37 @@
 def create_an_empty_array
+   []
+
   
 end
 
 def create_an_array
+  [1,2,3,4]
   
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array,element)
+ array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+    array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
+   array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+    array << element
   
 end
 
 def remove_element_from_start_of_array(array)
+   array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+    array << element
   
 end
 
